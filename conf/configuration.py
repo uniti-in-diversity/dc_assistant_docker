7cd18c5ebc5bf6a3f9250514327aab646a2cee40
maxim <dekin_m@mail.ru>
2020-09-14 15:14:09 +0300

    Initial commit

---
+++ conf/configuration.py
@@ -0,0 +1,43 @@
+import os
+import re
+import socket
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+#########################
+#                       #
+#   Required settings   #
+#                       #
+#########################
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(' ')
+
+DATABASE = {
+    'NAME': os.environ.get('DB_NAME', 'dcassistant'),    # Database name
+    'USER': os.environ.get('DB_USER', ''),               # PostgreSQL username
+    'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+                                                         # PostgreSQL password
+    'HOST': os.environ.get('DB_HOST', 'localhost'),      # Database server
+    'PORT': os.environ.get('DB_PORT', ''),               # Database port (leave blank for default)
+    'CONN_MAX_AGE': int(os.environ.get('DB_CONN_MAX_AGE', '300')), # Database connection persistence
+}
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+
+#########################
+#                       #
+#   Optional settings   #
+#                       #
+#########################
+
+BASE_PATH = os.environ.get('BASE_PATH', '')
+
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+
+PAGINATE_COUNT = int(os.environ.get('PAGINATE_COUNT', 5))
+
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
+
+TIME_ZONE = os.environ.get('TIME_ZONE', 'Europe/Moscow')
+
---
+++ docker-entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#stop if errors
+set -e
+
+#run as non-root users
+umask 002
+
+#try to connect to DB
+DB_WAIT_TIMEOUT=${DB_WAIT_TIMEOUT-3}
+MAX_DB_WAIT_TIME=${MAX_DB_WAIT_TIME-30}
+CUR_DB_WAIT_TIME=0
+while ! ./manage.py migrate 2>&1 && [ "${CUR_DB_WAIT_TIME}" -lt "${MAX_DB_WAIT_TIME}" ]; do
+  echo "⏳ Waiting on DB... (${CUR_DB_WAIT_TIME}s / ${MAX_DB_WAIT_TIME}s)"
+  sleep "${DB_WAIT_TIMEOUT}"
+  CUR_DB_WAIT_TIME=$(( CUR_DB_WAIT_TIME + DB_WAIT_TIMEOUT ))
+done
+if [ "${CUR_DB_WAIT_TIME}" -ge "${MAX_DB_WAIT_TIME}" ]; then
+  echo "❌ Waited ${MAX_DB_WAIT_TIME}s or more for the DB to become ready."
+  exit 1
+fi
+
+# Create Superuser if required
+
+if [ -z ${SUPERUSER_NAME} ]; then
+SUPERUSER_NAME='admin'
+fi
+if [ -z ${SUPERUSER_EMAIL} ]; then
+SUPERUSER_EMAIL='admin@example.com'
+fi
+if [ -z ${SUPERUSER_PASSWORD} ]; then
+SUPERUSER_PASSWORD='admin'
+fi
+
+  ./manage.py shell --interface python << END
+from django.contrib.auth.models import User
+if not User.objects.filter(username='${SUPERUSER_NAME}'):
+    u=User.objects.create_superuser('${SUPERUSER_NAME}', '${SUPERUSER_EMAIL}', '${SUPERUSER_PASSWORD}')
+END
+
+echo "💡 Superuser Username: ${SUPERUSER_NAME}, E-Mail: ${SUPERUSER_EMAIL}"
+
+./manage.py collectstatic --no-input
+
+echo "✅ Initialisation is done."
+
+exec $@
